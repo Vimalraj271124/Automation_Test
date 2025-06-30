c0df36a98dbdb60904dc227d543f80d37919e8d5
Vimalraj-2711 <uuama7188@gmail.com>
2025-06-30 15:08:52 +0530

    Add files via upload

---
+++ Test_9(By_Xpath1(type_text).py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.techlistic.com/p/selenium-practice-form.html")
+firstname=driver.find_element(By.XPATH,"(//input[@type='text'])[1]")
+firstname.clear()
+firstname.send_keys("vimal")
+time.sleep(2)
+
+secname=driver.find_element(By.XPATH,"(//input[@type='text'])[2]")
+secname.clear()
+secname.send_keys("raj")
+time.sleep(3)
+
+driver.quit()
